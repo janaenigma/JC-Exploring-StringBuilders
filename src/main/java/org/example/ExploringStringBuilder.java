94571f615f7005245a36417963755252056c541a
Jana Enigma Baruc <barucjana@gmail.com>
2025-08-07 16:32:15 +0800

    complete: Exploring Java StringBuilder

--- src/main/java/org/example/ExploringStringBuilder.java
+++ src/main/java/org/example/ExploringStringBuilder.java
@@ -1,6 +1,6 @@
 package org.example;
 
-public class Main {
+public class ExploringStringBuilder {
     public static void main(String[] args) {
 
         // Exercise 1: append() - Appending Content
